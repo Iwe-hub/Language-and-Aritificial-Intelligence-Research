565c83898ff8feac8de03f1e23db5a54acf095c9
Iwe-hub <bookmead@gmail.com>
2025-02-28 20:46:20 +0000

    Add files via upload

---
+++ GenerativeAI/extract.js
@@ -0,0 +1,11 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const scrape = async (page) => {
+    return await page.evaluate(async () => {
+        
+    })
+}
